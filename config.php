74c6ba019980d6a95453c90ec62b10a3c2d7aeab
mirko.caruso <mirko.caruso@jobrapido.com>
2025-03-20 15:25:33 +0100

    dev-cloud test first push

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configurazione del database
-$db_host = 'localhost';
+$db_host = '34.44.222.213';
 $db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_user = 'appmod-phpapp-user';
+$db_pass = '}Ef^z?-f;Ukf9+_n';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
